9c7a7ca91696c422c7d7b64c2fc89e285e524f2e
Marina Titova <marinatells@mail.ru>
2017-11-09 22:39:33 +0300

    + components/menu

---
+++ app/app.js
@@ -0,0 +1,23 @@
+(function () {
+	'use strict';
+
+	let data = {
+		title: 'Рабочие',
+		items: [
+			{
+				anchor: 'mail.ru'
+			},
+			{
+				anchor: 'yandex.ru'
+			},
+			{
+				anchor: 'google.com'
+			},
+		]
+	};
+
+	let menu = new Menu({
+		el: document.querySelector('.js-menu'),
+		data,
+	});
+})();
